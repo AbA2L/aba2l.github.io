1b8a067862244a17d8dfe554ad28f4827b461cdd
aba2l@email.com <aba2l@email.com>
2021-08-26 01:19:53 +0100

    BlogV1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210825.1547';
+const cacheName = 'chirpy-20210826.0110';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
